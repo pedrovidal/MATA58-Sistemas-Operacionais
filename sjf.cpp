dc3dc49ab831503b7e64d9fa5560db76c7ee0cd5
Gabriel Lecomte <gabriel1906@gmail.com>
2017-03-02 01:00:11 -0300

    Corrects SJF with non-preemptive approach

--- sjf.cpp
+++ sjf.cpp
@@ -19,19 +19,30 @@ void sjf(processo * vet, int numeroDeProcessos){
 		chegada.insert(make_pair(vet[i].chegada, vet[i]));
 	}
 	int time = 0;
+	processo * atual =  NULL;
 	pair<multimap<int, processo>::iterator, multimap<int, processo>::iterator> srch;
-	while (!chegada.empty() || !fila.empty()) {
+	while (!chegada.empty() || !fila.empty() || atual != NULL) {
 		srch = chegada.equal_range(time);
 		for (multimap<int, processo>::iterator it = srch.first; it != srch.second; it++) {
 			fila.push(&((*it).second));
 			chegada.erase(it);
 		}
-		if (!fila.empty()) {
-			fila.top()->tempo++;
-			cout << fila.top()->nome;
-			if (fila.top()->tempo == fila.top()->duracao) {
-				fim.insert(make_pair(fila.top()->nome, time - fila.top()->chegada + 1));
-				fila.pop();
+		if (atual != NULL) {
+			atual->tempo++;
+			cout << atual->nome;
+			if (atual->tempo == atual->duracao) {
+				fim.insert(make_pair(atual->nome, time - atual->chegada + 1));
+				atual = NULL;
+			}
+		}
+		else if (!fila.empty()) {
+			atual = fila.top();
+			fila.pop();
+			atual->tempo++;
+			cout << atual->nome;
+			if (atual->tempo == atual->duracao) {
+				fim.insert(make_pair(atual->nome, time - atual->chegada + 1));
+				atual = NULL;
 			}
 		}
 		else {
